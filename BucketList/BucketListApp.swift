02142d7ecad110f85c387959e977e92081a90f21
Marcus Benoit <benoit.marcus@gmail.com>
2024-06-29 18:42:26 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Marcus Benoit on 29.06.24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
